8d5d6a0766b94e10057f06a7678e9c27ee8acb56
Julia de Lorimier <jldelorimier@gmail.com>
2020-10-18 14:45:37 -0400

    updated index view method to use render() instead of HttpResponse()

--- polls/views.py
+++ polls/views.py
@@ -1,17 +1,26 @@
-from django.http import HttpResponse
-from django.template import loader
+from django.http import HttpResponse # <-- Won't need this once all the methods use render() instead of HttpResponse()
+from django.template import loader # <-- ¿Might not need this once all the methods use render() instead of HttpResponse()?
+from django.shortcuts import render # for NEW index view
 
-from .models import Question
+from .models import Question # for NEW index view
+
+# OLD INDEX VIEW
+# def index(request):
+#   latest_question_list = Question.objects.order_by('-pub_date')[:5]
+#   template = loader.get_template('polls/index.html')
+#   context = {
+#       'latest_question_list': latest_question_list,
+#   }
+#   # output = ', '.join([q.question_text for q in latest_question_list])
+#   # return HttpResponse(output)
+#   return HttpResponse(template.render(context, request))
+
+# NEW INDEX VIEW
 
 def index(request):
   latest_question_list = Question.objects.order_by('-pub_date')[:5]
-  template = loader.get_template('polls/index.html')
-  context = {
-      'latest_question_list': latest_question_list,
-  }
-  # output = ', '.join([q.question_text for q in latest_question_list])
-  # return HttpResponse(output)
-  return HttpResponse(template.render(context, request))
+  context = {'latest_question_list': latest_question_list}
+  return render(request, 'polls/index.html', context)
 
 def detail(request, question_id):
   return HttpResponse("You're looking at question %s." % question_id)

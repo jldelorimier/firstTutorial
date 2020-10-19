bdb9309ff3230025eabb2d218f31bf54fa548f93
Julia de Lorimier <jldelorimier@gmail.com>
2020-10-19 15:43:09 -0700

    updated detail.html so that the poll detail template contains an HTML form to actually vote; replaced dummy voting function with real voting function in polls/views.py under def vote() that increments vote count for that choice for that question; vote function redirects user to results page after they submit a vote

--- polls/views.py
+++ polls/views.py
@@ -1,10 +1,13 @@
 from django.http import HttpResponse # <-- Won't need this once all the methods use render() instead of HttpResponse()
+from django.http import HttpResponse, HttpResponseRedirect # <-- added this for vote view from part 4
 from django.template import loader # <-- ¿Might not need this once all the methods use render() instead of HttpResponse()?
 # from django.http import Http404 # used this first to create 404 exceptions, but now we're using get_objeft_or_404()
 # from django.shortcuts import render # used for revised index view; getting rid of this bc I think it's baked into get_object_or_404, render below
 from django.shortcuts import get_object_or_404, render
+from django.urls import reverse # <-- added this for vote view from part 4
 
 from .models import Question # for NEW index view
+from .models import Choice, Question # <-- does this need to be in a separate line from the line before? or are these lines redundant? added this for vote view from part 4. 
 
 # OLD INDEX VIEW
 # def index(request):
@@ -37,6 +40,11 @@ def results(request, question_id):
   return HttpResponse(response % question_id)
 
 def vote(request, question_id):
+  question = get_object_or_404(Question, pk=question_id)
+  try:
+    selected_choice = question.choice_set.get(pk=request.POST['choice'])
+  except (KeyError, Choice.DoesNotExist):
+    
   return HttpResponse("You're voting on question %s." % question_id)
 
 # Create your views here.
